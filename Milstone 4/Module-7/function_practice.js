1fb1d669cedf4fdbf6220647d717676c60d79c7b
Mamunfx <md01993049420@gmail.com>
2024-09-07 12:57:24 +0600

    function push

---
+++ Milstone 4/Module-7/function_practice.js
@@ -0,0 +1,64 @@
+// Task-1
+// Take four parameters. Multiply the four numbers and then return the result
+
+// function multi (a,b,c,d) {
+//     multiplied = a*b*c*d;
+//     return multiplied;
+// }
+// console.log(multi(2,3,4,5));
+
+
+//Task-2
+// Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+
+// function task2(num) {
+//     if (num%2!==0) {
+//         return num*2;
+//     }
+//     else{
+//         return num/2;
+//     }
+// }
+// console.log(task2(8));
+
+
+//Task-3
+// Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
+
+// function make_avg(numArra,divi) {
+//     avg = 0 ;
+//     for (const element of numArra) {
+//         avg = avg + element ; 
+//     }
+//     return avg/divi;
+// }
+// console.log(make_avg([12,2,40,30],4));
+
+
+// Task-4
+// Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
+
+// function count_zero(binaryString) {
+//     let a = binaryString.split("");
+//     count = 0;
+//     for (const element of a) {
+//         if(element==0){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// console.log(count_zero('101010000010011'));
+
+
+// Task-5
+// Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
+
+// function odd_even(num) {
+//     if (num%2==0) {
+//         return 'Even';
+//     } else {
+//         return 'Odd';
+//     }
+// }
+// console.log(odd_even(69));

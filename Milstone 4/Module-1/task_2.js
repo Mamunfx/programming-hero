a14e6db10dfbbe909c37ba62ab9c93ca2a39b444
Mamunfx <md01993049420@gmail.com>
2024-09-07 12:53:15 +0600

    Push

---
+++ Milstone 4/Module-1/task_2.js
@@ -0,0 +1,7 @@
+function avgMarks(a,b,c,d,e) {
+    let sumMarks = a+b+c+d+e ;
+    let avg = sumMarks/5;
+    return avg;
+}
+
+console.log(avgMarks(99,70,80,60,78).toFixed(2));
---
+++ Milstone 4/Module-1/task_3.js
@@ -0,0 +1,6 @@
+function remainder(num) {
+    let remain = num%5;
+    return remain;
+}
+
+console.log(remainder(119));

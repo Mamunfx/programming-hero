75f58c3264a3161c5b964cea046fd7efbe435d18
Mamunfx <md01993049420@gmail.com>
2024-09-07 12:55:00 +0600

    break push

---
+++ Milstone 4/Module-4/break.js
@@ -0,0 +1,24 @@
+//***************** break means literally stop the rest of the loop to execute **************
+
+
+//Write a loop 1 to 200. Use break to exit the loop once you find 100.
+
+// for (let i = 1; i <=200 ; i++) {
+//     console.log(i);
+    
+//     if (i==100) {
+//         break;
+//     }
+// }
+
+
+// Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+
+// let a = 1;
+// while (a<=200) {
+//     if (a==101) {
+//         break;
+//     }
+//     console.log(a);
+//     a++;
+// }

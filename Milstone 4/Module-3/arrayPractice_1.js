224599a701cdd2ab6a9146448575c3e86f14b431
Mamunfx <md01993049420@gmail.com>
2024-09-07 12:54:30 +0600

    Array push

---
+++ Milstone 4/Module-3/arrayPractice_1.js
@@ -0,0 +1,50 @@
+// TASK -- 1
+// let a = ['Apple' , 'Mango' , 'Ananas' , 'jackFruit' , 'DragonFruit']
+
+// console.log(a[2]);
+
+// a[1] = 'jambura'
+// console.log(a);
+
+
+// TASK--2
+
+// let B = ['coxs-bazar','St. Martin','Sajek'];
+
+// B.push('Mongla Bondor');
+// console.log(B);
+
+// B.push('Sylhet tea garden', 'Rangamati Hills')
+// console.log(B);
+
+// B.pop();
+// console.log(B);
+
+
+// TASK--3
+
+// let c = ['book-1','book-2','book-3','book-4'];
+
+// let d = c.includes('javasript book') ? 'Present':'Not Present' ;
+// console.log(d);
+
+
+// TASK--4
+
+// let e = 20;
+// let f = [12,3,100];
+
+// let g = Array.isArray(e)?'This is an array':'Its not an array';
+// let h = Array.isArray(f)?'This is an array':'Its not an array';
+// console.log(g);
+// console.log(h);
+
+
+// TASK--5
+
+// let x = ['Apple'];
+// let y = ['Juice'];
+
+// let z = x.concat(y);
+// console.log(x,y);
+// console.log(z);

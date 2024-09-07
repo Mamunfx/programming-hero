f240b1b6e245b5c0537aa0113da27be2b424fffd
Mamunfx <md01993049420@gmail.com>
2024-09-07 12:55:45 +0600

    continue push

---
+++ Milstone 4/Module-4/Loop_task_1.js
@@ -0,0 +1,53 @@
+// "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+// let i = 1;
+// while (i<=60) {
+//     console.log("I will invest at least 6 hrs every single day for next 60 days!", i);
+//     i++;
+// }
+
+
+/***Subtask-1:
+Find all the odd numbers from 61 to 100.*/
+/***Subtask-2:
+Find all the even numbers from 78 to 98.*/
+// let i = 61;
+// while (i<=100) {
+//     console.log(i);
+//     i+=2;
+// }
+
+// let a = 78;
+// while (a<=98) {
+//     console.log(a);
+//     a+=2;
+// }
+
+/***
+Subtask-1:
+Display sum of all the odd numbers from 81 to 131.*//***
+Subtask-2:
+Display sum of all the even numbers from 206 to 311.*/
+
+// let i = 81;
+// let sum = 0;
+// while (i<=131) {
+//     sum = sum + i;
+//     i+=2;
+// }
+// console.log(sum);
+
+// let a = 206;
+// sum=0;
+// while (a<=311) {
+//     sum +=a;
+//     a+=2;
+// }
+// console.log(sum);
+
+
+//Implement a countdown timer that counts down from 21 to 15.
+// let x = 21
+// while (x>=15) {
+//     console.log(x);
+//     x--;
+// }

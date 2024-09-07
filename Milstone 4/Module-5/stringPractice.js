652b318b8e815617621807917134cfdafa077278
Mamunfx <md01993049420@gmail.com>
2024-09-07 12:56:49 +0600

    string push

---
+++ Milstone 4/Module-5/stringPractice.js
@@ -0,0 +1,43 @@
+
+// Problem 1 and 2 :--
+
+const str = "JavaScript is great. I love JavaScript because JavaScript is ";
+// const wordToCount = new RegExp('a', 'g');
+// let repeated = str.match(wordToCount);
+// console.log(repeated.length);
+
+
+// let splitStr= str.split('');
+// let count = 0 ; 
+// let count2 = 0;
+// for (let i = 0; i < splitStr.length; i++) {
+//     if (splitStr[i]==='a') {
+//         count++;
+//     }
+// }
+// for (let j = 0; j < splitStr.length; j++) {
+//     if (splitStr[j]==='A') {
+//         count2++;
+//     }
+// }
+// console.log(`word 'a' exist ${count} times and word 'A' exist for ${count2} times .`);
+
+
+// Problem 3 :--
+
+// let isVowel= str.includes('a','e','i','o','u');
+// console.log(isVowel);
+
+
+// Problem 4 :--
+
+// let owo = 'I am x-men and you are xeron and she is isoxen !'
+// let newStr = owo.toLowerCase();
+
+// console.log(newStr.replaceAll('x', 'y'));
+
+
+// Problem 5 :--
+//Little bit advance.
+// let capitalized = str.replace(/\b\w/g, char => char.toUpperCase());
+// console.log(capitalized);

e1373a1d3816cc7bcfff29bb980acf1436744b03
Mamunfx <md01993049420@gmail.com>
2024-09-08 18:38:33 +0600

    practice part1

---
+++ Milstone 4/Module-8/practice1.js
@@ -0,0 +1,67 @@
+// Task-1:
+// Write a function to convert temperature from Celsius to Fahrenheit.
+
+// function celToFah(cel) {
+//     let fah = cel*(9/5)+32;
+//     return fah;
+// }
+// console.log(celToFah(100));
+
+// function fahToCel(fah) {
+//     let Cel = (fah - 32)*5/9;
+//     return Cel;
+// }
+// console.log(fahToCel(212));
+
+// Task-2:
+// You are given an array of numbers. Count how many times the a number is repeated in the array.
+
+// numbers = [5,6,11,12,98, 5]
+
+// function find(checkNum) {
+//     count = 0;
+//     for (const element of numbers) {
+//         if (checkNum==element) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// console.log(find(12));
+
+// Task-3:
+// Write a function to count the number of vowels in a string.
+
+// function vowelCount(str) {
+//     let lower = str.toLowerCase();
+//     let splitt = lower.split("");
+//     let count=0;
+//     for (const element of splitt) {
+//         if (element=='a'||element=='e'||element=='i'||element=='o'||element=='u') {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// console.log(vowelCount("Amar shonar Bangla ami tomay vhalo bashi !"));
+
+// Task-4:
+// Write a function to find the longest word in a given string.
+
+// function longestWord(str) {
+//   let firstSplit = str.split(" ");
+//   let testString = "";
+//   for (const element of firstSplit) {
+//     let a = element.length;
+//     let b = testString.length;
+//    if (a>b) {
+//     testString = element;
+//    }
+//   }
+//   return  testString;
+// }
+// console.log(longestWord("Amar shonar bangla ami tomay vhalobashi"));
+
+
+// Task-5:
+// Generate a random number between 10 to 20.

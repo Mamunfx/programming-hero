ca7f0c63aca88b593ee10af95998e18d0099a7b1
Mamunfx <md01993049420@gmail.com>
2024-09-08 18:34:17 +0600

    math push

---
+++ Milstone 4/Module-8/mathNote.js
@@ -0,0 +1,24 @@
+// const Math = {
+//     min: function min(num1, num2){
+//         return num2;
+//     }
+// }
+
+// console.log(Math.max);
+
+const min = Math.min(45, 21, 65, 99, 126, 5, -99)
+console.log(min);
+const max = Math.max(45, 21, 65, 99, 126, 5, -99, 987);
+console.log(max);
+
+console.log(Math.PI)
+console.log(Math.abs(5-10)) //give us the difference.mane durotto duita number er moddhe!
+console.log(Math.round(2.25)) // number round figure kore but 0.50 er niche thakle floor kore dey!
+console.log(Math.round(2.85)) // and 0.5 er upore thakle ceiling e pouchaia dey number re!
+console.log(Math.floor(2.95)) 
+console.log(Math.ceil(2.00001))
+
+console.log(Math.random())  
+console.log(Math.random()*10) // without multiply math.random will generate number in fraction !
+const rand = Math.round(Math.random()*10)
+console.log(rand)

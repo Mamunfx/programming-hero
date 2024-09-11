1347c0029bf0931b46e40b3d4b5d6a3112505b22
Mamunfx <md01993049420@gmail.com>
2024-09-11 08:50:38 +0600

    practice 2 push

---
+++ Milstone 4/Module-8/practice2.js
@@ -0,0 +1,20 @@
+// Task -1:
+// Find the lowest number in the array below.
+
+// const heights2 = [167, 190, 120, 165, 137];
+// function lowestNum(params) {
+//         let low = params[0];
+//     for (const element of params) {
+//         if (element<low) {
+//             low = element
+//         }
+//     }
+//     return low;
+// }
+// console.log(lowestNum(heights2));
+
+
+// Task -2:
+// Find the friend with the smallest name.
+
+const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];

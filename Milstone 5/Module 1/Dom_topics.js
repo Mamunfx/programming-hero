8e05f88fc65d2edf29e9266558e2fc1ae4c8dd5b
Mamunfx <md01993049420@gmail.com>
2024-09-16 17:08:24 +0600

    Dom first notes

---
+++ Milstone 5/Module 1/Dom_topics.js
@@ -0,0 +1,39 @@
+
+
+// Dynamic Style, GetAttribute, SetAttribute, InnerText, InnerHTML
+
+/* 
+<script>
+const liCollection = document.getElementsByTagName('li');
+// console.log(liCollection);
+for (const li of liCollection) {
+    // console.log(li.innerText);
+}
+
+// option-1: getElementsByTagName
+const allHeadings = document.getElementsByTagName('h1');
+for (const h1 of allHeadings) {
+    // console.log(h1.innerText);
+}
+
+// option-2: getElementById
+const fruitsTitle = document.getElementById('fruits-title');
+fruitsTitle.innerText = 'Fruits changed by JS';
+
+// option-3: getElementsByClassName
+const places = document.getElementsByClassName('important-places');
+for(const place of places){
+    // console.log(place.innerText)
+}
+
+// option-4: querySelector
+eita just first child return korbe !
+
+// option-5: querySelectorAll
+const someLi = document.querySelectorAll('.fruits-container li');
+// console.log(someLi);
+for(const li of someLi){
+    // console.log(li.innerText);
+}
+</script> 
+*/

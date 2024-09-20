18ac5e0e651bcae797683c6a6c55a9ea5fe93e4c
Mamunfx <md01993049420@gmail.com>
2024-09-20 18:32:56 +0600

    all

--- Milstone 5/Module 3/js/login.js
+++ Milstone 5/Module 3/js/login.js
@@ -1,14 +1,13 @@
 document.getElementById("logInBtn").addEventListener("click", function (event) {
     event.preventDefault();
-    const phone = document.getElementById("phone").value;
-    const pin = document.getElementById("pin").value;
+    const phone = document.getElementById("phone00").value;
+    const pin = document.getElementById("pin00").value;
 
-    if (phone==="5" && pin==="12345") {
-        console.log("you are loged in");
-        
+    if (phone==="5" && pin==="1234") {
+        console.log("kiree");
+        window.location.assign('other.html');
     }
     else {
         console.log("get out");
-        
     }
 })

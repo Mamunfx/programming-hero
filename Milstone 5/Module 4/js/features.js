282eeceb87e1bb546f76aecf9bc2846821f44de1
Mamunfx <md01993049420@gmail.com>
2024-09-21 15:12:17 +0600

    feature

---
+++ Milstone 5/Module 4/js/features.js
@@ -0,0 +1,10 @@
+document.getElementById("show-add-money-form").addEventListener('click',function(){
+    toggle("add-money-form");
+});
+document.getElementById("show-cash-out-form").addEventListener('click',function (){
+    toggle("cash-out-form");
+});
+document.getElementById("show-transaction-history").addEventListener('click',function(){
+    toggle("transaction-section");
+});
+

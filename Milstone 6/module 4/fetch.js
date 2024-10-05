17d340574d5b4b5596015552147f6394a5c8600d
Mamunfx <md01993049420@gmail.com>
2024-10-05 09:11:58 +0600

    fetching

--- Milstone 6/module 4/fetch.js
+++ Milstone 6/module 4/fetch.js
@@ -5,11 +5,32 @@ function loadusers() {
 }
 
 
+function loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data2 => postPrinterHandler(data2))
+}
+
 function  namePrinterHandler(data) {
     const ul = document.getElementById("unordered")
     for (const user of data) {
         const li = document.createElement('li');
         li.innerText = user.name;
         ul.appendChild(li);
+    }
+}
+
+
+function postPrinterHandler(data2) {
+    const div = document.getElementById("posts");
+    for (const post of data2) {
+        console.log(post);
+        
+        const divInner = document.createElement('div')
+        divInner.innerHTML=`
+        <h1> ${post.id}</h1>
+        <h1> ${post.body}</h1>
+        `;
+        div.appendChild(divInner);
     }
 }

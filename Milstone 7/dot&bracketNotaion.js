4fa51f575a763e134129db014d15126ff769e447
Mamunfx <md01993049420@gmail.com>
2024-10-16 15:33:53 +0600

    dot and bracket notaion of object

---
+++ Milstone 7/dot&bracketNotaion.js
@@ -0,0 +1,13 @@
+let obj= {
+    names:'aul',
+    24:'24',
+    'eu-ro':45
+}
+
+console.log(obj.names); // dot notaion works with normal keys but not with kerpa keys..
+// console.log(obj.'eu-ro'); //  but not with kerpa keys.. it wil throw error..
+
+console.log(obj[24]); // but bracket notaion works with every object keys ..
+
+
+

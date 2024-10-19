b92b156dd8b85663dd18a66bdc7c8276a2898f74
Mamunfx <md01993049420@gmail.com>
2024-10-19 20:34:05 +0600

    ordekh

--- Milstone 7/module 3/my-secondReact-app/src/App.jsx
+++ Milstone 7/module 3/my-secondReact-app/src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import StateManage from './StateManage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   function events() {
     alert('show alert')
@@ -20,7 +18,7 @@ function App() {
       <button onClick={event2}>BUtton2</button>      
       <button onClick={()=>{alert('button 3 clicked')}}>BUtton3</button>      
       <button onClick={()=>{event2()}}>BUtton4</button>      
-
+      <StateManage></StateManage>
     </>
   )
 }
--- Milstone 7/module 3/my-secondReact-app/src/StateManage.jsx
+++ Milstone 7/module 3/my-secondReact-app/src/StateManage.jsx
@@ -2,12 +2,20 @@ import { useState } from "react"
 
 export default function StateManage() {
     const [totalCount,CountFunction]=useState(0)
+    function add() {
+        const newtotalCount =totalCount+1;
+        return CountFunction(newtotalCount)
+    }
+    function remove() {
+        const newtotalCount =totalCount-1;
+        return CountFunction(newtotalCount)
+    }
 
     return(
         <div>
             <h1>Total number of count : {totalCount}</h1>
-            <button onClick={add}></button>
-            <button onClick={remove}></button>
+            <button onClick={add}>ADD</button>
+            <button onClick={remove}>REMOVE</button>
         </div>
     )
 }

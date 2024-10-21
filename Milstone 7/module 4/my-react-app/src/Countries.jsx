e0961daf193b64b73d2fd0c4a0c22224eb997fc7
Mamunfx <md01993049420@gmail.com>
2024-10-22 00:10:53 +0600

    countries

--- Milstone 7/module 4/my-react-app/src/Countries.jsx
+++ Milstone 7/module 4/my-react-app/src/Countries.jsx
@@ -1,9 +1,22 @@
 import { useEffect, useState } from "react";
+import Country from "./Country";
 
 export default function Countries() {
     const [countries , setCountries]=useState([]);
 
-    useEffect({
-        
-    })
+    
+    useEffect(()=>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res=>res.json())
+        .then(data=>setCountries(data))
+    },[])
+
+    return(
+        <div>
+            <h1>length : {countries.length}</h1>;
+            {
+                countries.map(country => <Country key={country.cca3} country={country}></Country>)
+            }
+        </div>
+    )
 }

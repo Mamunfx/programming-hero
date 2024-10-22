a0992ff477d4ea9c68d07ca7862e9c70e1e861c3
Mamunfx <md01993049420@gmail.com>
2024-10-22 13:55:15 +0600

    module 4 done

--- Milstone 7/module 4/my-react-app/src/Countries.jsx
+++ Milstone 7/module 4/my-react-app/src/Countries.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import Country from "./Country";
-
+import './Box.css'
 export default function Countries() {
     const [countries , setCountries]=useState([]);
+
 
     
     useEffect(()=>{
@@ -11,12 +12,16 @@ export default function Countries() {
         .then(data=>setCountries(data))
     },[])
 
+
     return(
         <div>
             <h1>length : {countries.length}</h1>;
+            
+            <div className="boxcss">
             {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                countries.map(country => <Country key={country.cca3} country={country} className="indivuji"></Country>)
             }
+            </div>
         </div>
     )
 }
--- Milstone 7/module 4/my-react-app/src/Country.jsx
+++ Milstone 7/module 4/my-react-app/src/Country.jsx
@@ -1,5 +1,27 @@
+import { useState } from 'react';
+import './Box.css'
+
 export default function Country({country}) {
+    console.log(country)
+    const {name}=country;
+    const {flags}=country;
+
+    const {visited,setVisited}=useState(false);
+    const [addedCountry, setAddingCountry]=useState([])
+
+    const handVisited = () => {
+        setVisited(!visited);
+    }
+    const handleAdding = (nam)=>{
+        setAddingCountry(nam);
+    }
     return(
-        <h3>Name : {country.cca2}</h3>
+        <div className="indivuji">
+         <h3>Name : {name.common}</h3>
+         <img src={flags.png}></img>
+         <button onClick={handVisited}>visited</button>
+         {visited ? 'visited' : 'not visited'}
+                <button>Country added </button>
+        </div>
     )
 }

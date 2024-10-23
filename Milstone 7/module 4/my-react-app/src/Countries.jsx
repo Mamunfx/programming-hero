70c956ef61c2f673ecb738f4e5e932252b517eee
Mamunfx <md01993049420@gmail.com>
2024-10-23 23:23:02 +0600

    new project

--- Milstone 7/module 4/my-react-app/src/Countries.jsx
+++ Milstone 7/module 4/my-react-app/src/Countries.jsx
@@ -4,8 +4,6 @@ import './Box.css'
 export default function Countries() {
     const [countries , setCountries]=useState([]);
 
-
-    
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res=>res.json())

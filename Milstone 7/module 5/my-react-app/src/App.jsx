2695baa759efc0dde802c56337eb4edba856c0e2
Mamunfx <md01993049420@gmail.com>
2024-10-24 14:06:21 +0600

    assets added

--- Milstone 7/module 5/my-react-app/src/App.jsx
+++ Milstone 7/module 5/my-react-app/src/App.jsx
@@ -6,15 +6,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      
+    <h1 className='text-2xl font-bold'>Knowledge Cafe</h1>
       
     </>
   )

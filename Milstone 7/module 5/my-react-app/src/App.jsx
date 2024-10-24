1865ee6b706c1f8d9de1c5dabb0f8ac1a66c005f
Mamunfx <md01993049420@gmail.com>
2024-10-24 15:35:04 +0600

    changed assets

--- Milstone 7/module 5/my-react-app/src/App.jsx
+++ Milstone 7/module 5/my-react-app/src/App.jsx
@@ -1,14 +1,18 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Blogs from './Blogs'
 
 function App() {
 
   return (
     <>
-      
+    <header className='w-11/12 mx-auto flex justify-between items-center pb-4 mb-4 border-b-2'>
     <h1 className='text-2xl font-bold'>Knowledge Cafe</h1>
-      
+    <img src="../images/profile.png"  className='w-12'/>
+    </header>
+    <main className='w-11/12 mx-auto grid grid-cols-3'>
+    <Blogs ></Blogs>
+    <div className='border'><p>Lorem, ipsum.</p></div>
+    </main>
     </>
   )
 }
---
+++ Milstone 7/module 5/my-react-app/src/Blogs.jsx
@@ -0,0 +1,9 @@
+const Blogs = () => {
+    return (
+        <div className='col-span-2 border'>
+            <h1>Hello cafe</h1>
+        </div>
+    );
+};
+
+export default Blogs;

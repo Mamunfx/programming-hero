ed4034ffaa372154e506ce77592f2e9e57225336
Mamunfx <md01993049420@gmail.com>
2024-11-01 22:20:44 +0600

    form with state

--- Milstone 8/module 3/react-core-3/src/App.jsx
+++ Milstone 8/module 3/react-core-3/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import SimpleForm from './components/SimpleForm'
+import StatefulForm from './components/StatefulForm'
 
 function App() {
   return (
     <>
-      
       <h1>Vite + React</h1>
-      
+      <SimpleForm></SimpleForm>
+      <StatefulForm></StatefulForm>
     </>
   )
 }
---
+++ Milstone 8/module 3/react-core-3/src/components/SimpleForm.jsx
@@ -0,0 +1,24 @@
+
+const SimpleForm = () => {
+    const handleForm =e=>{
+        e.preventDefault();
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        console.log(e.target.password.value);
+        console.log(e.target.name.value);
+        
+    }
+    return (
+        <form onSubmit={handleForm} >
+            <input type="text" name="name" placeholder="name"/>
+                <br />
+                <input type="email" name="email" placeholder="email" />
+                <br />
+                <input type="password" name="password" placeholder="pass"/>
+                <br />
+                <input type="submit" value="Submit" />
+        </form>
+    );
+};
+
+export default SimpleForm;

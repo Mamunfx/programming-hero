d6c7d1ef2e2d1d347da734fed43954672fb1c30f
Mamunfx <md01993049420@gmail.com>
2024-11-02 19:47:57 +0600

    basic layout done

---
+++ Milstone 8/module 4/name-of-your-project/src/components/ErrorPage.jsx
@@ -0,0 +1,10 @@
+
+const ErrorPage = () => {
+    return (
+        <div className="flex justify-center items-center text-2xl">
+            <h1>Page not found </h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- Milstone 8/module 4/name-of-your-project/src/main.jsx
+++ Milstone 8/module 4/name-of-your-project/src/main.jsx
@@ -1,17 +1,34 @@
-import { StrictMode } from 'react'
+import { Children, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Root from './Root/Root'
 
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import ErrorPage from './components/ErrorPage';
+import Homepage from './components/Homepage';
+import Dashboard from './components/Dashboard';
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>,
+    children:[
+      {
+        path:"/",
+        element:<Homepage></Homepage>
+      },
+      {
+        path:"dashboard",
+        element:<Dashboard></Dashboard>
+      }
+    ]
   },
+  
 ]);
 
 createRoot(document.getElementById('root')).render(

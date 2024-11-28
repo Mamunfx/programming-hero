3702eac5bb988284b15a45a84de9add89f9afb03
Mamunfx <md01993049420@gmail.com>
2024-11-28 11:34:58 +0600

    restarting

--- Milstone 10/module 2/name-of-your-project/src/App.jsx
+++ Milstone 10/module 2/name-of-your-project/src/App.jsx
@@ -4,11 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleForm=(e)=>{
+    e.preventDefault();
+
+    const form =event.target;
+    const name= form.name.vlaue;
+    const email= form.email.value;
+    const user= {name,email};
+    console.log(user);
+  }
 
   return (
     <>
       <h1>Simple database</h1>
+      <form className='my-14 space-y-4' onSubmit={handleForm}>
+        <input type="text" name='name' placeholder='name' className='border p-3' />
+        <br />
+        <input type="email" name='email' placeholder='email' className='border p-3'/>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     </>
   )
 }

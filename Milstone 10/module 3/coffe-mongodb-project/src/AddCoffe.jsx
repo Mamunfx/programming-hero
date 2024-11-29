f7d5f02e9a195d5f105ad9d9f1d79023fc8863fe
Mamunfx <md01993049420@gmail.com>
2024-11-30 01:22:06 +0600

    okay

--- Milstone 10/module 3/coffe-mongodb-project/src/AddCoffe.jsx
+++ Milstone 10/module 3/coffe-mongodb-project/src/AddCoffe.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const AddCoffe = () => {
     return (
-        <div>
+        <div className='w-11/12 mx-auto'>
             <h1>ADD Coffe</h1>
+            <form className='space-y-2'>
+
+                <input type="text" name="Name" placeholder='Coffe name' className='border-2 p-4' />
+                <br />
+                <input type="text" name="quantity" placeholder='Quantity available' className=' border-2 p-4'/>
+            </form>
         </div>
     );
 };

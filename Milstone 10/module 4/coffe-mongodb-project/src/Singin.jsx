34a012570b26e32e04d316493dcedd197db95e8c
Mamunfx <md01993049420@gmail.com>
2024-12-02 08:53:48 +0600

    initialized auth intrigation in database

---
+++ Milstone 10/module 4/coffe-mongodb-project/src/Singin.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Singin = () => {
+    return (
+        <div>
+            <h1>Sing in Page : </h1>
+        </div>
+    );
+};
+
+export default Singin;<h1>Sing in Page : </h1>
